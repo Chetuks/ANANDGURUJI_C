fbb37c3590e8d2c82fbcd4d53e970c5ca1bb2749
Chethan <chetuks>
2018-09-22 17:28:07 +0530

    imageadded

--- app/src/main/java/com/ananada/addme/neutrinos/anandaguruji/FetchNextURLInterface.java
+++
@@ -1,9 +0,0 @@
-package com.ananada.addme.neutrinos.anandaguruji;
-
-/**
- * Created by Aviansh Raj  on 18/11/17.
- */
-
-public interface FetchNextURLInterface {
-    void onSuccessResonse(String downloadSuccessfulRespose, String fileType);
-}
